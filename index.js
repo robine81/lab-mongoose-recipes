7d306f9354ece979f286f89579ff54cf93ab000e
robine81 <robine.westberg@gmail.com>
2023-04-21 01:03:54 +0200

    I tried as much as I couldon all iterations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false)
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
@@ -7,6 +8,16 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const addRecipe = {
+  title: 'Swedish meatballs',
+  level: 'Amateur Chef',
+  ingredients: ['Minced_Beef', 'Onion', 'Breadcumbs', 'Egg'],
+  cuisine: 'Swedish',
+  dishType: 'Main_Course',
+  duration: 30,
+  creator: 'Robine',
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -14,10 +25,28 @@ mongoose
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
+  })  
+  //iteration 2
+  
+.then(() => {
+  return Recipe.create(addRecipe);
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  //iteration 3
+ .then(() => {
+  return Recipe.insertMany(data)
+ })
+
+ .then(()=> {
+  const filter = [{title: "Rigatoni alla Genovese"}]
+  const update = {duration:100}
+  return Recipe.findByIdAndUpdate(filter, update)
+ })
+
+ .then(() => {
+  console.log("Updated")
+ })
+
+ .catch(error => {
+  console.error('Error connecting to the database', error);
+});
